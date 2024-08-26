7400ddba974533850939a591c2e6110a03752027
vinuesanicolas <51464172+vinuesanicolas@users.noreply.github.com>
2024-08-26 18:54:26 -0300

    Add files via upload
    Desafío Día 2 ONE-ALURA

---
+++ app2.js
@@ -0,0 +1,17 @@
+
+/*Debe pedir al usuario responder 3 preguntas:
+
+ - ¿Cuál es tu nombre?
+- ¿Cuántos años tienes?
+- ¿Qué lenguaje de programación estás estudiando?
+Al final mostrará:
+"Hola [nombre], tienes [edad] años y ya estás aprendiendo [lenguaje]!"
+*/
+
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuál es tu edad?");
+let lenguaje = prompt("¿Qué lenguaje de programación estas estudiando?");
+
+//mostrar mensaje
+console.log(`Hola ${nombre}, tenés ${edad} años y estás estudiando ${lenguaje}!`);
+
